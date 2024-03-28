03b49edef941d92caa51034633f7d12b90b02bf6
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-28 15:04:16 +0000

    changes in the navLink

--- src/components/ManageHelpRequestsNavBar/ManageHelpRequestsNavBar.jsx
+++ src/components/ManageHelpRequestsNavBar/ManageHelpRequestsNavBar.jsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import "./ManageHelpRequestsNavBar.css"
+import { Link } from 'react-router-dom';
 
 const ManageHelpRequestsNavBar = ({ requestsActive, receivedOffersActive, helpOffersActive }) => {
     return (
@@ -12,21 +13,21 @@ const ManageHelpRequestsNavBar = ({ requestsActive, receivedOffersActive, helpOf
                     <Col sm={3}>
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item>
-                                <Nav.Link eventKey="first" className={requestsActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"} href="/create_request">My Requests</Nav.Link>
+                                <Nav.Link as={Link} to="/create_request" className={requestsActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"}>My Requests</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
                     <Col sm={3}>
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item>
-                                <Nav.Link eventKey="first" className={receivedOffersActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"} href="/request_management/received_offers">My Received Offers</Nav.Link>
+                                <Nav.Link as={Link} to="/request_management/received_offers" className={receivedOffersActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"} >My Received Offers</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
                     <Col sm={3}>
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item>
-                                <Nav.Link eventKey="first" className={helpOffersActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"} href="/request_management/my_offers">My Help Offers</Nav.Link>
+                                <Nav.Link as={Link}  to="/request_management/my_offers" className={helpOffersActive ? "btn-format btn-format-active" : "btn-format btn-format-inactive"}>My Help Offers</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
